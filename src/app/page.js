e6207d87c638f031b4598832c092ff8c222f6f16
Jovy Cortez <jovycortez11@gmail.com>
2024-02-19 15:16:56 -0500

    Products and ProductDetails page updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
+import { Hero } from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Products from "@/components/Products";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <Hero />
     <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-24">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
         <Products />
       </div>
     </main>
+    </>
 
   );
 }
