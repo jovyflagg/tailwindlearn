70536d8e685b46b243e72d2cf6d5529706223458
Jovy Cortez <jovycortez11@gmail.com>
2024-02-18 21:48:33 -0500

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import 'flowbite';
+import { Hero } from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          <Hero />
+          {children}
+          {/* <Footer /> */}
+        </body>
+
+      </html>
+    </>
   );
 }
